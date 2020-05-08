dab6c787046bfd6c5591540aac09ce56420e8d90
Aman Agrawal <aman.agrawal@ad.loginradius.net>
2020-05-08 11:00:17 +0530

    Added Readme file into the repository

--- NodeJs/FacebookAuthenticationPassport/routes.js
+++ NodeJs/FacebookAuthenticationPassport/routes.js
@@ -1,19 +1,16 @@
-var passport = require('passport');
+const passport = require('passport');
 const express = require('express');
-
 var router = express.Router();
-
 
 router.get('/', function (req, res) {
   res.render('pages/index.ejs'); // load the index.ejs file
 });
+
 router.get('/profile', isLoggedIn, function (req, res) {
- console.log("req ", req.user);
   res.render('pages/profile.ejs', {
     user: req.user // get the user out of session and pass to template
   });
 });
-
 
 router.get('/error', isLoggedIn, function (req, res) {
   res.render('pages/error.ejs');
