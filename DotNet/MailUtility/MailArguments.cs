9dcf19b74aa0fe844f18c38672fa0eaeaa3b6a66
hemant404 <56862469+hemant404@users.noreply.github.com>
2020-11-18 14:32:04 +0530

    Code added for send emails through SMTP in dotnet.

---
+++ DotNet/MailUtility/MailArguments.cs
@@ -0,0 +1,16 @@
+﻿namespace MailUtility
+{
+    //MailArguments which is used in sending an email
+    public class MailArguments
+    {
+        public string MailTo { get; set; }
+        public string Bcc { get; set; }
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public string SmtpHost { get; set; }
+        public string Password { get; set; }
+        public int Port { get; set; }
+        public string MailFrom { get; set; }
+    }
+}
---
+++ DotNet/README.md
@@ -0,0 +1,10 @@
+## How to send emails through C#/.NET using SMTP
+
+## Prerequisites
+* Basic knowledge about the C#/.NET and it's application IDE
+
+## How to Start
+1. Clone the repo.
+2. Open `EmailSendingUtility.sln` file in IDE.
+3. Add the required information to send the Email in `Program.cs` file.
+4. Build and Run the Project.
