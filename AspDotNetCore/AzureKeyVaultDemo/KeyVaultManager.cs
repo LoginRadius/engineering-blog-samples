cac55c0ed9ea59fcf2a92ff36c84362aec76b700
Joydip Kanjilal <joydip.kanjilal@c2dsoft.com>
2021-06-29 18:27:52 +0530

    title: Initial Checkin\n Article on Azure Key Vault

---
+++ AspDotNetCore/AzureKeyVaultDemo/KeyVaultManager.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Security.KeyVault.Secrets;
+using System.Threading.Tasks;
+
+namespace AzureKeyVaultDemo
+{
+    public class KeyVaultManager: IKeyVaultManager
+    {
+        private readonly SecretClient _secretClient;
+        public KeyVaultManager(SecretClient secretClient)
+        {
+            _secretClient = secretClient;
+        }
+
+        public async Task<string> GetSecret(string secretName)
+        {
+            try
+            {
+                KeyVaultSecret keyValueSecret = await _secretClient.GetSecretAsync(secretName);
+                return keyValueSecret.Value;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
