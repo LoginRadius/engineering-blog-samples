dc5d7d412886592eafa4f18304146f892203d666
Viral Prajapati <viral.prajapati@esparkbizmail.com>
2021-12-20 13:15:02 +0530

    initial commit

---
+++ php/public/validate.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use Firebase\JWT\JWT;
+
+require_once('../vendor/autoload.php');
+
+if (! preg_match('/Bearer\s(\S+)/', $_SERVER['HTTP_AUTHORIZATION'], $matches)) {
+    header('HTTP/1.0 400 Bad Request');
+    echo 'Token not found in request';
+    exit;
+}
+
+$jwt = $matches[1];
+if (! $jwt) {
+    // No token was able to be extracted from the authorization header
+    header('HTTP/1.0 400 Bad Request');
+    exit;
+}
+
+$secretKey  = 'bGS6lzFqvvSQ8ALbOxatm7/Vk7mLQyzqaS34Q4oR1ew=';
+$token = JWT::decode($jwt, $secretKey, ['HS512']);
+$now = new DateTimeImmutable();
+$serverName = "your.domain.name";
+
+if ($token->iss !== $serverName ||
+    $token->nbf > $now->getTimestamp() ||
+    $token->exp < $now->getTimestamp())
+{
+    header('HTTP/1.1 401 Unauthorized');
+    exit;
+}
+
+// Show the page
