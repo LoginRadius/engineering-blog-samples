39669e49643d53a20bab7545db0afc5f5d66c556
Uma Victor <63749911+uma-victor1@users.noreply.github.com>
2022-01-18 12:27:37 +0100

    Node,js auth guide sample code - Uma Victor

---
+++ NodeJs/NodejsAuthenticationGuide/db.js
@@ -0,0 +1,14 @@
+const Mongoose = require("mongoose");
+
+const localDB = `mongodb://localhost:27017/role_auth`;
+
+const connectDB = async () => {
+  await Mongoose.connect(localDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  console.log("MongoDB Connected");
+};
+
+module.exports = connectDB;
---
+++ NodeJs/NodejsAuthenticationGuide/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const connectDB = require("./db");
+const app = express();
+const cookieParser = require("cookie-parser");
+const { adminAuth, userAuth } = require("./middleware/auth.js");
+
+const PORT = 5000;
+
+app.set("view engine", "ejs");
+
+connectDB();
+
+app.use(express.json());
+app.use(cookieParser());
+
+// Routes
+app.use("/api/auth", require("./Auth/route"));
+
+app.get("/", (req, res) => res.render("home"));
+app.get("/register", (req, res) => res.render("register"));
+app.get("/login", (req, res) => res.render("login"));
+app.get("/logout", (req, res) => {
+  res.cookie("jwt", "", { maxAge: "1" });
+  res.redirect("/");
+});
+app.get("/admin", adminAuth, (req, res) => res.render("admin"));
+app.get("/basic", userAuth, (req, res) => res.render("user"));
+
+const server = app.listen(PORT, () =>
+  console.log(`Server Connected to port ${PORT}`)
+);
+
+process.on("unhandledRejection", (err) => {
+  console.log(`An error occurred: ${err.message}`);
+  server.close(() => process.exit(1));
+});
