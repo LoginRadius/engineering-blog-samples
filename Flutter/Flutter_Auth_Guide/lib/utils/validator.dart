494f29b6065c3f240342b35b4d8ec5278a896753
iloveteajay <atytjani@gmail.com>
2022-02-02 14:20:50 +0100

    Implementing User Authentication and Registration in Flutter

---
+++ Flutter/Flutter_Auth_Guide/lib/utils/validator.dart
@@ -0,0 +1,56 @@
+class Validator {
+  static String? validateEmail(String value) {
+    Pattern pattern = r'^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+';
+    RegExp regex =  RegExp(pattern as String);
+    if (!regex.hasMatch(value)) {
+      return '🚩 Please enter a valid email address.';
+    } else {
+      return null;
+    }
+  }
+
+  static String? validateDropDefaultData(value) {
+    if (value == null) {
+      return 'Please select an item.';
+    } else {
+      return null;
+    }
+  }
+
+  static String? validatePassword(String value) {
+    Pattern pattern = r'^.{6,}$';
+    RegExp regex =  RegExp(pattern as String);
+    if (!regex.hasMatch(value)) {
+      return '🚩 Password must be at least 6 characters.';
+    } else {
+      return null;
+    }
+  }
+
+  static String? validateName(String value) {
+    if (value.length < 3) {
+      return '🚩 Username is too short.';
+    } else {
+      return null;
+    }
+  }
+
+  static String? validateText(String value) {
+    if (value.isEmpty) {
+      return '🚩 Text is too short.';
+    } else {
+      return null;
+    }
+  }
+
+
+
+  static String? validatePhoneNumber(String value) {
+    if (value.length != 11) {
+      return '🚩 Phone number is not valid.';
+    } else {
+      return null;
+    }
+  }
+
+}
