ddd63d6511f19fad85e28051ebe035e20abb79f0
iloveteajay <atytjani@gmail.com>
2022-02-02 14:20:50 +0100

    Implementing User Authentication and Registration in Flutter

---
+++ Flutter/Flutter_Auth_Guide/lib/core/api_client.dart
@@ -0,0 +1,84 @@
+import 'package:dio/dio.dart';
+import 'package:loginradius_example/core/constants.dart';
+
+class ApiClient {
+  final Dio _dio = Dio();
+
+  Future<dynamic> registerUser(Map<String, dynamic>? data) async {
+    try {
+      Response response = await _dio.post(
+          'https://api.loginradius.com/identity/v2/auth/register',
+          data: data,
+          queryParameters: {'apikey': ApiSecret.apiKey},
+          options: Options(headers: {'X-LoginRadius-Sott': ApiSecret.sott}));
+      return response.data;
+    } on DioError catch (e) {
+      return e.response!.data;
+    }
+  }
+
+  Future<dynamic> login(String email, String password) async {
+    try {
+      Response response = await _dio.post(
+        'https://api.loginradius.com/identity/v2/auth/login',
+        data: {
+          'email': email,
+          'password': password,
+        },
+        queryParameters: {'apikey': ApiSecret.apiKey},
+      );
+      return response.data;
+    } on DioError catch (e) {
+      return e.response!.data;
+    }
+  }
+
+  Future<dynamic> getUserProfileData(String accessToken) async {
+    try {
+      Response response = await _dio.get(
+        'https://api.loginradius.com/identity/v2/auth/account',
+        queryParameters: {'apikey': ApiSecret.apiKey},
+        options: Options(
+          headers: {'Authorization': 'Bearer $accessToken'},
+        ),
+      );
+      return response.data;
+    } on DioError catch (e) {
+      return e.response!.data;
+    }
+  }
+
+  Future<dynamic> updateUserProfile({
+    required String accessToken,
+    required Map<String, dynamic> data,
+  }) async {
+    try {
+      Response response = await _dio.put(
+        'https://api.loginradius.com/identity/v2/auth/account',
+        data: data,
+        queryParameters: {'apikey': ApiSecret.apiKey},
+        options: Options(
+          headers: {'Authorization': 'Bearer $accessToken'},
+        ),
+      );
+      return response.data;
+    } on DioError catch (e) {
+      return e.response!.data;
+    }
+  }
+
+  Future<dynamic> logout(String accessToken) async {
+    try {
+      Response response = await _dio.get(
+        'https://api.loginradius.com/identity/v2/auth/access_token/InValidate',
+        queryParameters: {'apikey': ApiSecret.apiKey},
+        options: Options(
+          headers: {'Authorization': 'Bearer $accessToken'},
+        ),
+      );
+      return response.data;
+    } on DioError catch (e) {
+      return e.response!.data;
+    }
+  }
+}
---
+++ Flutter/Flutter_Auth_Guide/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:loginradius_example/screens/register.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'LoginRadius Example',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const RegisterScreen(),
+    );
+  }
+}
