4e1abe15f2bafe4afdf09fb77e44406f6ab7f3cc
icode247 <zionekekenta@gmail.com>
2022-07-25 07:44:08 +0100

    fixed database issues

--- Deno/denoAPI_JWT_Auth/src/database/connectDB.ts
+++ Deno/denoAPI_JWT_Auth/src/database/connectDB.ts
@@ -1,26 +1,7 @@
 import { MongoClient } from "https://deno.land/x/mongo@v0.30.0/mod.ts";
 
-  const client = new MongoClient();
-
-  // Connecting to a Mongo Atlas Database
-  await client.connect({
-    db: 'deno_auth',
-    tls: true,
-    servers: [
-      {
-        host: "dbhost",
-        port: 27017,
-      },
-   ],
-    credential: {
-      username: "dbName",
-      password: "dbPassword",
-      db: "admin",
-      mechanism: "SCRAM-SHA-1",
-    },
-  });
-
- console.log("Database connected!");
- const db = client.database("deno_auth");
-
- export default db;
+const dbString = `DB_STRING`;
+const client = new MongoClient();
+await client.connect(dbString);
+const db = client.database("deno_auth");
+export default db;
